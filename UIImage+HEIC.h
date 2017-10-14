38bd02f716c4dd1eabb42ad299231a8f60607c95
Tim Johnsen <timonus@gmail.com>
2017-10-14 10:12:06 -0700

    Add UIGraphicsImageRenderer extensions.

--- UIImage+HEIC.h
+++ UIImage+HEIC.h
@@ -13,4 +13,18 @@ NS_ASSUME_NONNULL_BEGIN
 /// Returns and NSData HEIC representation of the image if possible, otherwise returns nil.
 NSData *_Nullable tj_UIImageHEICRepresentation(UIImage *const image, const CGFloat compressionQuality);
 
+@interface UIGraphicsImageRenderer (TJHEICAdditions)
+
+- (nullable NSData *)tj_HEICDataWithCompressionQuality:(const CGFloat)compressionQuality
+                                               actions:(NS_NOESCAPE UIGraphicsImageDrawingActions)actions;
+
+- (nullable NSData *)tj_HEICDataWithCompressionQuality:(const CGFloat)compressionQuality
+           fallingBackToJPEGDataWithCompressionQuality:(const CGFloat)jpegCompressionQuality
+                                               actions:(NS_NOESCAPE UIGraphicsImageDrawingActions)actions;
+
+- (nullable NSData *)tj_HEICDataFallingBackToPNGDataWithCompressionQuality:(const CGFloat)compressionQuality
+                                                                   actions:(NS_NOESCAPE UIGraphicsImageDrawingActions)actions;
+
+@end
+
 NS_ASSUME_NONNULL_END
--- UIImage+HEIC.m
+++ UIImage+HEIC.m
@@ -28,3 +28,47 @@ NSData *_Nullable tj_UIImageHEICRepresentation(UIImage *const image, const CGFlo
     return imageData;
 }
 
+@implementation UIGraphicsImageRenderer (TJHEICAdditions)
+
+- (nullable NSData *)tj_HEICDataWithCompressionQuality:(const CGFloat)compressionQuality
+                                               actions:(NS_NOESCAPE UIGraphicsImageDrawingActions)actions
+{
+    return [self tj_HEICDataWithCompressionQuality:compressionQuality
+                                           actions:actions
+                                          fallback:nil];
+}
+
+- (nullable NSData *)tj_HEICDataWithCompressionQuality:(const CGFloat)compressionQuality
+           fallingBackToJPEGDataWithCompressionQuality:(const CGFloat)jpegCompressionQuality
+                                               actions:(NS_NOESCAPE UIGraphicsImageDrawingActions)actions
+{
+    return [self tj_HEICDataWithCompressionQuality:compressionQuality
+                                           actions:actions
+                                          fallback:^NSData *(UIImage *image) {
+                                              return UIImageJPEGRepresentation(image, jpegCompressionQuality);
+                                          }];
+}
+
+- (nullable NSData *)tj_HEICDataFallingBackToPNGDataWithCompressionQuality:(const CGFloat)compressionQuality
+                                                                   actions:(NS_NOESCAPE UIGraphicsImageDrawingActions)actions
+{
+    return [self tj_HEICDataWithCompressionQuality:compressionQuality
+                                           actions:actions
+                                          fallback:^NSData *(UIImage *image) {
+                                              return UIImagePNGRepresentation(image);
+                                          }];
+}
+
+- (nullable NSData *)tj_HEICDataWithCompressionQuality:(const CGFloat)compressionQuality
+                                               actions:(NS_NOESCAPE UIGraphicsImageDrawingActions)actions
+                                              fallback:(NSData *(^)(UIImage *))fallback
+{
+    UIImage *const image = [self imageWithActions:actions];
+    NSData *data = tj_UIImageHEICRepresentation(image, compressionQuality);
+    if (data.length == 0 && fallback) {
+        data = fallback(image);
+    }
+    return data;
+}
+
+@end
